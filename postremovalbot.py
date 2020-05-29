906129fe9821b1bcae8198708c80ce1c1c40a6ae
AkitotheExiled <myguestmike@gmail.com>
2020-05-28 23:09:12 -0700

    V1.1 - Added Headers and Footers for comment replies

--- postremovalbot.py
+++ postremovalbot.py
@@ -6,7 +6,7 @@ import time
 
 class PostRemovalBot():
     def __init__(self):
-        self.user_agent = "PostRemovalBot / V1.1 by ScoopJr"
+        self.user_agent = "PostRemovalBot / V1.2 by ScoopJr"
         print('Starting up...', self.user_agent)
         CONFIG = ConfigParser()
         CONFIG.read('config.ini')
@@ -15,7 +15,7 @@ class PostRemovalBot():
         self.client = CONFIG.get('main', 'CLIENT_ID')
         self.secret = CONFIG.get('main', 'SECRET')
         self.subreddit = CONFIG.get('main', 'SUBREDDIT')
-        self.type = CONFIG.get('main', 'TYPE')
+        #self.type = CONFIG.get('main', 'TYPE')
         self.type = "public"
         self.token_url = "https://www.reddit.com/api/v1/access_token"
         self.token = ""
@@ -29,6 +29,9 @@ class PostRemovalBot():
                                   username=self.user)
         self.get_json_file()
         self.keys = self.flair_and_reason.keys()
+        self.header = CONFIG.get('MSG', 'HEADER')
+        self.footer = CONFIG.get('MSG', 'FOOTER')
+        self.footer2 = CONFIG.get('MSG', 'FOOTER2')
 
     def get_json_file(self):
         """Gets removal reason json file"""
@@ -48,6 +51,13 @@ class PostRemovalBot():
         self.token = response2.json()['access_token']
         self.t_type = response2.json()['token_type']
 
+    def format_removal_message(self, author_name, removal_reason):
+        try:
+            header = self.header.format(author=author_name)
+            message = str(header + "\n\n" + removal_reason + "\n\n" + self.footer + "\n\n" + self.footer2)
+        except author_name or removal_reason is None:
+            print("You must specify a removal reason or author name!")
+        return message
 
     def main(self):
         while True:
@@ -57,7 +67,8 @@ class PostRemovalBot():
                     break
                 if post.link_flair_text in self.keys:
                     post.mod.remove()
-                    post.mod.send_removal_message(self.flair_and_reason[post.link_flair_text], type=self.type)
+                    msg = self.format_removal_message(author_name=post.author.name, removal_reason=self.flair_and_reason[post.link_flair_text])
+                    post.mod.send_removal_message(msg, type=self.type)
                     print(f"Removed: {post.name}")
             print(f"...Taking a small break!  Be back in {self.delay} seconds")
             time.sleep(self.delay)
